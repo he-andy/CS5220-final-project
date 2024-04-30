ba645aec2b2ca590f0c0cc4c47539c57bb08d8d2
Bryant Park <bryantpark04@gmail.com>
2024-04-29 18:36:57 -0700

    Implement random paths generation and integrate into main

--- common.h
+++ common.h
@@ -7,4 +7,5 @@ double ls_american_put_option_backward_pass(std::vector<std::vector<double>> &X,
                                             std::vector<double> &t, double r,
                                             double strike);
 // void simulate()
+std::vector<std::vector<double>> generate_random_paths(int n_paths, int n_time_steps, double initial_price, double delta_t, double drift, double volatility);
 #endif
