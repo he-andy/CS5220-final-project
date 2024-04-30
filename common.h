d5b7d4e97f23ecbc085ef473a5a584b8363212e7
Andy He <ah734@login10.chn.perlmutter.nersc.gov>
2024-04-29 19:01:36 -0700

    add a vector printer

--- common.h
+++ common.h
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <vector>
 #ifndef __CS267_COMMON_H__
 #define __CS267_COMMON_H__
@@ -7,4 +8,15 @@ double ls_american_put_option_backward_pass(std::vector<std::vector<double>> &X,
                                             std::vector<double> &t, double r,
                                             double strike);
 // void simulate()
+//
+template <typename T> void printVector(const std::vector<T> &vec) {
+  std::cout << "[";
+  for (size_t i = 0; i < vec.size(); i++) {
+    std::cout << vec[i];
+    if (i < vec.size() - 1) {
+      std::cout << ", ";
+    }
+  }
+  std::cout << "]" << std::endl;
+}
 #endif
